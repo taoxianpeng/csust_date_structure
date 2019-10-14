55314fd8214c8b67df2774e424e918035ef75cf8
taoxianpeng <492350627@qq.com>
2019-10-14 18:47:30 +0800

    t2

---
+++ 2019/t2-1.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+
+int Find(int *list,int length, int num){
+    int left=0, right=length-1;
+    int insert_pos;
+    int mid=-1;
+    while(left<=right){
+        mid=left+(right-left)/2;
+        if(list[mid] == num)
+            return 1;
+        if(list[mid] >num)
+            right = mid-1;
+        if(list[mid]< num)
+            left = mid+1;
+    }
+    if(mid!=-1){
+        if(list[mid]>num) 
+            insert_pos=mid;
+        else
+            insert_pos=mid+1;                
+        for(int i=length-1; i>=insert_pos; i--){
+            list[i+1]=list[i];
+        }
+        list[insert_pos]=num;
+        return 2;  
+    }
+    return 0;
+
+}
+int main()
+{
+    int list[100] = {1,2,3,4,5,6,7,9,11,12};
+    int elemt = 13;
+    int count = 10;
+    int sign = Find(list,count,elemt);
+    switch (sign)
+    {
+    case 0:
+        cout << "list is empty!" << endl;
+        break;
+    case 1:
+        cout << "find !" << endl;
+        break;
+    case 2:
+        cout << "don't find,this elemt"<<"("<< elemt << ")"<<" has inserted into list!"<<endl;
+        count++;
+        break;
+    default:
+        cout << "error "+sign <<endl;
+        break;
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        cout << list[i];
+        if(i < count-1) cout << " ";
+    }
+    
+    return 0;
+}
