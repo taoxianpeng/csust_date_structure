a9e02e938517aad6568d843559ac6318e0784642
taoxianpeng <492350627@qq.com>
2019-11-24 23:28:14 +0800

    新增 统计二叉树结点函数

--- 2018/t3.cpp
+++ 2018/t3.cpp
@@ -73,10 +73,30 @@ int getWeight(BTree *s){
     }
     return result;
 }
+int count_all(BTree *k){
+    //递归统计结点数
+    if(k==NULL) 
+        return 0;
+    int Ln=count_all(k->lchild);
+    int Rn=count_all(k->rchild);
+    return Ln+Rn+1;
+}
+
+int count_leaf(BTree *k){
+    //递归统计叶子结点
+    if(k==NULL) return 0;
+    if(k->lchild == NULL && k->rchild ==NULL)
+        return 1;
+    int Ln=count_leaf(k->lchild);
+    int Rn=count_leaf(k->rchild);
+    return Ln+Rn;
+    
+}
 int main()
 {
     Elemtype st[100] = {'1','2','3','4','5','6','7','8','9','x'};
     int size = 10;
+    int sum = 0 ;
     BTree *s=new BTree();
     level_creatBTree(s,st,size);
     pre_visit(s);
@@ -86,5 +106,10 @@ int main()
     cout << "max hight of btree is: " << h << endl;
     cout << "max weight of btree is: " << w << endl;
 
+
+    sum = count_all(s);
+    cout << "sum of all elem is : " << sum << endl;
+    int s2 = count_leaf(s);
+    cout << "sum of leaf elem is : " << s2 << endl;
     return 0;
 }
